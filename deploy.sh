8d6a1caca3ea46118e3e6922e47d0d8d57b7ca44
ben <ben@ELs-MacBook-Pro.local>
2025-04-07 10:52:38 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,8 +20,13 @@ cd -
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git add -f dist
+git init
+git add -A
 git commit -m 'deploy'
 
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git subtree push --prefix dist origin gh-pages 
+git push -f git@github.com:jorel777/lendlens.git main:gh-pages
+# git push -f https://github.com/jorel777/lendlens.git main:gh-pages 
