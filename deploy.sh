124b4fe4a5b463eca3a3e0760f21036b69f07cde
ben <ben@ELs-MacBook-Pro.local>
2025-04-07 11:16:00 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -28,5 +28,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jorel777/lendlens.git main:gh-pages
-# git push -f https://github.com/jorel777/lendlens.git main:gh-pages 
+git push -f https://github.com/jorel777/lendlens.git main:gh-pages 
